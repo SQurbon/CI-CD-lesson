d2cbfa0ab9926ca141a83043ab77356c283fd91c
“SQurbon” <qurbonsultonqulov466@gmail.com>
2025-08-12 16:09:47 +0500

    navbar is already done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+    </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <nav>
+        <a href="">Logo</a>
+        <ul>
+            <li>
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="">About</a>
+            </li>
+            <li>
+                <a href="">Contact</a>
+            </li>
+            <li>
+                <a href="">Location</a>
+            </li>
+            <li>
+                <a href="">Register</a>
+            </li>
+            <li>
+                <a href="">Login</a>
+            </li>
+        </ul>
+        <button>bosing</button>
+    </nav>
+  )
+}
